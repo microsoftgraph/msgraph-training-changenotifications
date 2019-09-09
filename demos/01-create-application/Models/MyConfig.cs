c666ece57068cf359918de4c2c6922f0127831bb
Rob Windsor <rob.windsor@gmail.com>
2019-09-08 20:03:51 -0400

    Update project files for Demo 1

---
+++ demos/01-create-application/Models/MyConfig.cs
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+namespace msgraphapp
+{
+    public class MyConfig
+    {
+        public string AppId { get; set; }
+        public string AppSecret { get; set; }
+        public string TenantId { get; set; }
+        public string Ngrok { get; set; }
+    }
+}
